8d19a55e2a55840786212cadc2740c9503e5a93a
Kunal Dixit <kunaldixit15049@gmail.com>
2025-04-07 12:03:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
+getSumBtn.setAttribute("class", "btn")
 document.body.appendChild(getSumBtn);
+
+let table = document.querySelector("table");
 
 const getSum = () => {
 //Add your code here
+	const items = document.querySelectorAll(".price");
+  let sum = 0;
+  items.forEach((item, i) => {
+    sum += parseInt(item.textContent);
+  });
+  table.innerHTML += `
+  <tr>
+    <td colspan = 2>${sum} </th>
+  </tr> 
+  `;
   
 };
 
